a82058893ad4d1c5f89f6470812833ad42e3171f
khademul mowla <khademulmowla31eee@gmail.com>
2024-09-24 01:53:27 +0600

    committed app.js and updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+// function of show and hide 
+function showSectionById(id) {
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('transaction-history').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
+// add and remove background-color of button//
+const buttonDonation = document.getElementById('show-donation');
+buttonDonation.addEventListener('click', function () {
+    showSectionById('donation-section');
+    buttonDonation.classList.add('bg-green');
+    buttonHistory.classList.remove('bg-green');
+
+})
+const buttonHistory = document.getElementById('show-history');
+buttonHistory.addEventListener('click', function () {
+    showSectionById('transaction-history');
+    buttonHistory.classList.add('bg-green');
+    buttonDonation.classList.remove('bg-green');
+})
+// toggle blog //
+document.getElementById('blog-info').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = './blog.html'
+})
 // function of getting input field of write donation amount //
 function getInputAmount(id) {
     const inputField = document.getElementById(id).value;
@@ -11,6 +36,10 @@ function getTextValue(id) {
     const textValueAmount = parseFloat(textValue);
     return textValueAmount;
 }
+
+const btnCon = document.getElementById('btn-donate-con');
+const btnCon2 = document.getElementById('btn-donate2-con');
+const btnCon3 = document.getElementById('btn-donate3-con');
 // noakhali donation//
 document.getElementById('btn-noa').addEventListener('click', function (event) {
     event.preventDefault();
@@ -18,7 +47,8 @@ document.getElementById('btn-noa').addEventListener('click', function (event) {
     const totalNoa = getTextValue('total-amount-noa');
     const availableAmount = getTextValue('available-balance');
     if (isNaN(inputNoa) || inputNoa < 0 || inputNoa > availableAmount || availableAmount <= 0) {
-        return alert('Failed to Donate Money')
+        document.getElementById('my_modal_1').close();
+        return alert('Please provide valid data');
     }
     const newTotalNoa = totalNoa + inputNoa;
     document.getElementById('total-amount-noa').innerText = newTotalNoa;
@@ -28,30 +58,36 @@ document.getElementById('btn-noa').addEventListener('click', function (event) {
     const dateString = currentDate.toString();
     const div = document.createElement('div');
     div.innerHTML = `<div class="border-2 border-gray w-8/12 mx-auto p-8 rounded-lg mb-10 space-y-2">
-    <p class="text-lg font-bold">${newTotalNoa} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
-    <p class="text-sm text-gray-500">${dateString}</p>
+    <p class="text-lg font-bold">${newTotalNoa} Taka is Donated for Noakhali, Bangladesh</p>
+    <p class="text-sm text-gray-500">Date: ${dateString}</p>
     </div>`;
     document.getElementById('transaction-history').appendChild(div);
-
-
     const newAvailableAmout = availableAmount - inputNoa;
     document.getElementById('available-balance').innerText = newAvailableAmout;
     document.getElementById('input-amount-noa').value = "";
-
 })
-
 // feni donation //
 document.getElementById('btn-feni').addEventListener('click', function (event) {
     event.preventDefault();
-    const inputNoa = getInputAmount('input-amount-feni');
-    const totalNoa = getTextValue('total-amount-feni');
+    const inputFeni = getInputAmount('input-amount-feni');
+    const totalFeni = getTextValue('total-amount-feni');
     const availableAmount = getTextValue('available-balance');
-    if (isNaN(inputNoa) || inputNoa < 0 || inputNoa > availableAmount || availableAmount <= 0) {
-        return alert('Failed to Donate Money')
+    if (isNaN(inputFeni) || inputFeni < 0 || inputFeni > availableAmount || availableAmount <= 0) {
+        document.getElementById('my_modal_2').close();
+        return alert('Please provide valid data');
     }
-    const newTotalNoa = totalNoa + inputNoa;
-    document.getElementById('total-amount-feni').innerText = newTotalNoa;
-    const newAvailableAmout = availableAmount - inputNoa;
+    const newTotalFeni = totalFeni + inputFeni;
+    document.getElementById('total-amount-feni').innerText = newTotalFeni;
+    // add to transaction history //
+    const currentDate = new Date();
+    const dateString = currentDate.toString();
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="border-2 border-gray w-8/12 mx-auto p-8 rounded-lg mb-10 space-y-2">
+    <p class="text-lg font-bold">${newTotalFeni} Taka is Donated for Feni, Bangladesh</p>
+    <p class="text-sm text-gray-500">Date: ${dateString}</p>
+    </div>`;
+    document.getElementById('transaction-history').appendChild(div);
+    const newAvailableAmout = availableAmount - inputFeni;
     document.getElementById('available-balance').innerText = newAvailableAmout;
     console.log('clear')
     document.getElementById('input-amount-feni').value = "";
@@ -60,44 +96,40 @@ document.getElementById('btn-feni').addEventListener('click', function (event) {
 // quota movement //
 document.getElementById('btn-quota').addEventListener('click', function (event) {
     event.preventDefault();
-    const inputNoa = getInputAmount('input-amount-quota');
-    const totalNoa = getTextValue('total-amount-quota');
+    const inputQuota = getInputAmount('input-amount-quota');
+    const totalQuota = getTextValue('total-amount-quota');
     const availableAmount = getTextValue('available-balance');
-    if (isNaN(inputNoa) || inputNoa < 0 || inputNoa > availableAmount || availableAmount <= 0) {
-        return alert('Failed to Donate Money')
+    if (isNaN(inputQuota) || inputQuota < 0 || inputQuota > availableAmount || availableAmount <= 0) {
+        document.getElementById('my_modal_3').close();
+        return alert('Please provide valid data');
     }
-    const newTotalNoa = totalNoa + inputNoa;
-    document.getElementById('total-amount-quota').innerText = newTotalNoa;
-    const newAvailableAmout = availableAmount - inputNoa;
+    const newTotalQuota = totalQuota + inputQuota;
+    document.getElementById('total-amount-quota').innerText = newTotalQuota;
+    // add to transaction history //
+    const currentDate = new Date();
+    const dateString = currentDate.toString();
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="border-2 border-gray w-8/12 mx-auto p-8 rounded-lg mb-10 space-y-2">
+    <p class="text-lg font-bold">${newTotalQuota} Taka is Donated for injured brave individuals in Quota Movement, Bangladesh</p>
+    <p class="text-sm text-gray-500">Date: ${dateString}</p>
+    </div>`;
+    document.getElementById('transaction-history').appendChild(div);
+    const newAvailableAmout = availableAmount - inputQuota;
     document.getElementById('available-balance').innerText = newAvailableAmout;
-    console.log('clear')
     document.getElementById('input-amount-quota').value = "";
 
 })
+// modal handler //
 
-
-
-
-// function of show and hide 
-function showSectionById(id) {
-    document.getElementById('donation-section').classList.add('hidden');
-    document.getElementById('transaction-history').classList.add('hidden');
-    document.getElementById(id).classList.remove('hidden');
-}
-function handleBtn(event) {
-
-}
-// show and hide 
-const buttonDonation = document.getElementById('show-donation');
-buttonDonation.addEventListener('click', function () {
-    showSectionById('donation-section');
-    buttonDonation.classList.add('bg-green');
-    buttonHistory.classList.remove('bg-green');
-
+btnCon.addEventListener('click', function () {
+    const modal = document.getElementById('my_modal_1');
+    modal.close();
+});
+btnCon2.addEventListener('click', function () {
+    const modal = document.getElementById('my_modal_2');
+    modal.close();
 })
-const buttonHistory = document.getElementById('show-history');
-buttonHistory.addEventListener('click', function () {
-    showSectionById('transaction-history');
-    buttonHistory.classList.add('bg-green');
-    buttonDonation.classList.remove('bg-green');
+btnCon3.addEventListener('click', function () {
+    const modal = document.getElementById('my_modal_3');
+    modal.close();
 })
